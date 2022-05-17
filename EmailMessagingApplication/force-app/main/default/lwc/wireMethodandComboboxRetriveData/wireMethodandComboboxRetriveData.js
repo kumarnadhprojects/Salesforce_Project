bc0afab11016e8e754acdf42362345bf5f71138f
Kumar <kumarnadhprojects@gmail.com>
2022-05-17 22:06:47 +0530

    Some chsnges in code

--- EmailMessagingApplication/force-app/main/default/lwc/wireMethodandComboboxRetriveData/wireMethodandComboboxRetriveData.js
+++ EmailMessagingApplication/force-app/main/default/lwc/wireMethodandComboboxRetriveData/wireMethodandComboboxRetriveData.js
@@ -1,17 +1,17 @@
 import { LightningElement, track, wire} from 'lwc';
 import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import ACCOUNT_OBJECT from '@salesforce/schema/Account';
-import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+import STUDENT_OBJECT from '@salesforce/schema/Student_school__c';
+import STUDENT_BATCHES_FIELD from '@salesforce/schema/Student_school__c.Batches__c';
 
 export default class WireMethodandComboboxRetriveData extends LightningElement {
     @track value;
 
-    @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+    @wire(getObjectInfo, { objectApiName: STUDENT_OBJECT })
     objectInfo;
 
-    @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: INDUSTRY_FIELD})
-    IndustryPicklistValues;
+    @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: STUDENT_BATCHES_FIELD})
+    BatchPicklistValues;
 
     handleChange(event) {
         this.value = event.detail.value;
