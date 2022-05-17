e9a81d8e3fcb76e92f5ca5f18f04c0fe8b287124
Kumar <kumarnadhprojects@gmail.com>
2022-05-17 21:43:23 +0530

    New component added

---
+++ EmailMessagingApplication/force-app/main/default/lwc/wireMethodandComboboxRetriveData/wireMethodandComboboxRetriveData.js
@@ -0,0 +1,19 @@
+import { LightningElement, track, wire} from 'lwc';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+
+export default class WireMethodandComboboxRetriveData extends LightningElement {
+    @track value;
+
+    @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+    objectInfo;
+
+    @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: INDUSTRY_FIELD})
+    IndustryPicklistValues;
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+}
